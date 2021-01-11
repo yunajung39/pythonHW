e34c83d514c79a4b417f5836252f665762ad9c74
yunajung <noobol700601@cau.ac.kr>
2021-01-11 15:16:28 +0900

    18.6 심사문제: 두 수 사이의 숫자 중 3으로 끝나지 않는 숫자 출력하기

---
+++ 18.6 심사문제.py
@@ -0,0 +1,12 @@
+start, stop = map(int, input().split())
+
+i = start
+ 
+while True:
+    if istop:
+        break
+    if i%10==3:
+        i+=1
+        continue
+    print(i, end=' ')
+    i += 1
