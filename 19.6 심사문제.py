c7220b619924bab661fca6b54c88cd871a679338
yunajung <noobol700601@cau.ac.kr>
2021-01-11 15:17:26 +0900

    19.6 심사문제: 산 모양으로 별 출력하기

---
+++ 19.6 심사문제.py
@@ -0,0 +1,12 @@
+a=int(input())
+
+for i in range(a):
+    for j in reversed(range(a)):
+        if j > i:
+            print(' ', end='')
+        else:
+            print('*', end='')
+    for j in range(a):
+        if j < i:
+            print('*', end='')
+    print()
